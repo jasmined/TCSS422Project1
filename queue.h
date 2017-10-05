9b331a4b0b42129cb17e91cb9c83cd83267ae1e3
Jasmine Dacones <jasminedacones@gmail.com>
2017-10-04 20:52:25 -0700

    Added queue header file.

---
+++ queue.h
@@ -0,0 +1,19 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include "pcb.h"
+
+typedef struct node {
+	PCB *pcb;
+	struct node *next;
+} Node;
+
+typedef struct queue {
+	Node *head;
+	int size, counter;
+}
+
+// Functions
+
+int isEmpty();
+
