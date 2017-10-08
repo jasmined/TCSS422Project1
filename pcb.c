c3cb24ffeb524a9e048e87efdade4af169866480
Jasmine Dacones <jasminedacones@gmail.com>
2017-10-07 19:02:29 -0700

    PCB contents print to text file.

--- pcb.c
+++ pcb.c
@@ -2,15 +2,8 @@
 #include <stdlib.h>
 #include "pcb.h"
 
-unsigned int pid = 0;
+unsigned int pid = -1;
 
-
-// int main(void) {
-	// PCB_p pcb = construct();
-	// setPID(pcb);
-	// toString(pcb);
-	// toString_context(pcb);
-// }
 
 char* getStateString(enum state_type state) {
 	switch(state) {
@@ -34,13 +27,6 @@ char* getStateString(enum state_type state) {
 
 PCB_p construct() {
 	PCB_p pcb = (PCB_p) malloc(sizeof(pcb));
-	if (pcb == NULL) {
-		printf("pcb allocated");
-	}
-	// pcb->pid = pid;
-	// pcb->priority = 1;
-	// pcb->state = new;
-	// pcb->context->pc = 0;
 	return pcb;
 }
 
@@ -53,18 +39,19 @@ void setPID(PCB_p pcb) {
 	pcb->pid = pid + 1;
 }
 
-char toString(PCB_p pcb) {
-	// printf("%d\n", pcb->pid);
+char toString(FILE *f, PCB_p pcb) {
+	printf("PCB #%d-----------------------------------------------------------------\n", pcb->pid);
 	printf("PID: %02d, Priority: %02u, State: %s\n", pcb->pid, pcb->priority, getStateString(pcb->state));
-	// printf("PCB #%d----------------------------------------------------------------------\n"
-	// "PID: %d, Priority: %u, State: %s, PC: %02d\n", 
-	// pcb->pid, pcb->pid, pcb->priority, getStateString(pcb->state), pcb->context->pc);
+	
+	fprintf(f, "\n\nPCB #%d----------------------------------------------------------------------\r\n", pcb->pid);
+	fprintf(f, "\nPID: %02d, Priority: %02u, State: %s\r\n", pcb->pid, pcb->priority, getStateString(pcb->state));
+
 }
 
-char toString_context(PCB_p pcb) {
-	printf("PC: 0, IR: 0, PSR: 0, r0: 0, r1: 0, r2: 0, r3: 0, r4: 0, r5: 0, r6: 0, r7: 0\n");
-	printf("----------------------------------------------------------------------------\n");
+char toString_context(FILE *f, PCB_p pcb) {
+	printf("PC: 0, IR: 0, PSR: 0, r0: 0, r1: 0, r2: 0, r3: 0, r4: 0, r5: 0, r6: 0, r7: 0\n\n");
+	fprintf(f, "PC: 0, IR: 0, PSR: 0, r0: 0, r1: 0, r2: 0, r3: 0, r4: 0, r5: 0, r6: 0, r7: 0\r\n");
+	fprintf(f, "\r\n");
 }
-	
-	
+
 	
--- pcb_test.c
+++ pcb_test.c
@@ -5,25 +5,31 @@
 
 int main(void) {
 	
+	FILE *f;
+	
+	f = fopen("pcb_out.txt", "w");
+	
 	PCB_p pcb_array[15];
 
 	PCB_p pcb = construct();
 	pcb->priority = rand() % 16;
 	setPID(pcb);
-	toString(pcb);
-	toString_context(pcb);
-	// deconstruct(pcb);
+	toString(f, pcb);
+	toString_context(f, pcb);
 	
-	// printf("------------------------\n");
-	for (int n = 0; n < 15; n++) {
+	
+	for (int n = 1; n < 15; n++) {
 		PCB_p pcb = construct();
 		pcb->pid = n;
 		pcb->priority = rand() % 16;
 		pcb->state = new;
 		pcb_array[n] = pcb;
-		toString(pcb_array[n]);
-		toString_context(pcb_array[n]);
+		toString(f, pcb_array[n]);
+		toString_context(f, pcb_array[n]);
+		// toFile(f, pcb);
 	}
-	// printf("------------------------\n");
+
+	
+	deconstruct(pcb);
 	
 }
